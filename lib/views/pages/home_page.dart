78bc31310de31fe55c8da6052b9aeefa79cbbcef
ABHILESH SINGH <sabhilesh260@gmail.com>
2025-03-07 09:24:16 +0530

    Updated some minor UI, and give the user option to report an issue and request a feature for the app

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -38,6 +38,10 @@ class HomePage extends StatelessWidget {
     ["X (Twitter) Videos", "xVideosBlockerNotifier", "assets/images/x.png"],
   ];
   final githubUrl = "https://github.com/ABHILESH1412/sankalp";
+  final issuesUrl =
+      "https://docs.google.com/forms/d/e/1FAIpQLScvWRgABpLPKN50RcnjE78CuPq66ML7XKiLMzHjm2EgvGv_2Q/viewform?usp=dialog";
+  final featureUrl =
+      "https://docs.google.com/forms/d/e/1FAIpQLSdOcPQ9CQfu9INH4e8gYyoupSTb3AvA3p6f4wTKIlMjQmA0Bg/viewform?usp=dialog";
 
   Future<void> _launchGitHub() async {
     final Uri url = Uri.parse(githubUrl);
@@ -45,6 +49,24 @@ class HomePage extends StatelessWidget {
       await launchUrl(url, mode: LaunchMode.externalApplication);
     } else {
       throw 'Could not launch $githubUrl';
+    }
+  }
+
+  Future<void> _reportAnIssue() async {
+    final Uri url = Uri.parse(issuesUrl);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $issuesUrl';
+    }
+  }
+
+  Future<void> _requestAFeature() async {
+    final Uri url = Uri.parse(featureUrl);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $featureUrl';
     }
   }
 
@@ -57,24 +79,60 @@ class HomePage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
         ),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // leading: const Icon(Icons.light),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: InkWell(
-              onTap: _launchGitHub,
-              child: Image.asset(
-                'assets/images/github.png', // Replace with your GitHub logo image
-                width: 30, // Adjust size
-                height: 30,
+        // toolbarHeight: 70,
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text(
+                textAlign: TextAlign.center,
+                "Settings",
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          IconButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
+            Divider(
+              thickness: 2,
+            ),
+            // Dark Mode and Light Mode
+            ValueListenableBuilder(
+              valueListenable: darkModeNotifier,
+              builder: (context, darkMode, child) {
+                return ListTile(
+                  leading: darkMode
+                      ? const Icon(Icons.dark_mode)
+                      : const Icon(Icons.light_mode),
+                  title: Text(
+                    darkMode ? "Dark Mode" : "Light Mode",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  onTap: () {
+                    SharedPrefsHelper()
+                        .setBool('darkModeNotifier', !darkModeNotifier.value);
+                    darkModeNotifier.value = !darkModeNotifier.value;
+                  },
+                  trailing: Switch(
+                      value: darkMode,
+                      onChanged: (value) {
+                        SharedPrefsHelper().setBool(
+                            'darkModeNotifier', !darkModeNotifier.value);
+                        darkModeNotifier.value = !darkModeNotifier.value;
+                      }),
+                );
+              },
+            ),
+            // App Info
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text(
+                "App Info",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
                     actions: [
                       TextButton(
                         onPressed: () {
@@ -97,30 +155,49 @@ class HomePage extends StatelessWidget {
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("1) This app will not automatically start on restart of the phone.", style: TextStyle(fontSize: 18),),
-                        SizedBox(height: 10,),
-                        Text("2) The app automatically detects videos playing on the home feed of X (twitter), Linkedin, Instagram and mutes them for a seamless, distraction-free experience.", style: TextStyle(fontSize: 18),),
+                        Text(
+                          "The app automatically detects videos playing on the home feed of X (twitter), Linkedin, Instagram and mutes them for a seamless, distraction-free experience.",
+                          style: TextStyle(fontSize: 18),
+                        ),
                       ],
-                    )),
-              );
-            },
-            icon: Icon(Icons.info),
-          ),
-          ValueListenableBuilder(
-            valueListenable: darkModeNotifier,
-            builder: (context, darkMode, child) {
-              return IconButton(
-                onPressed: () {
-                  SharedPrefsHelper()
-                      .setBool('darkModeNotifier', !darkModeNotifier.value);
-                  darkModeNotifier.value = !darkModeNotifier.value;
-                },
-                icon: darkMode ? Icon(Icons.light_mode) : Icon(Icons.dark_mode),
-              );
-            },
-          ),
-        ],
-        toolbarHeight: 70,
+                    ),
+                  ),
+                );
+              },
+            ),
+            // Report an Issue
+            ListTile(
+              leading: const Icon(Icons.bug_report_outlined),
+              title: const Text(
+                "Report an Issue",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: _reportAnIssue,
+            ),
+            // Request a Feature
+            ListTile(
+              leading: const Icon(Icons.star),
+              title: const Text(
+                "Request a Feature",
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: _requestAFeature,
+            ),
+            // Github
+            ListTile(
+              leading: Image.asset(
+                'assets/images/github.png', // Replace with your GitHub logo image
+                width: 25, // Adjust size
+                height: 25,
+              ),
+              title: Text(
+                "Github",
+                style: const TextStyle(fontSize: 18),
+              ),
+              onTap: _launchGitHub,
+            ),
+          ],
+        ),
       ),
       body: SafeArea(
         child: Column(
